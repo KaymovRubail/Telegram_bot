2be9f6a042ff665e3c497963ddcf64ce830e93d7
KaymovRubail <ibragimovjoni593@gmail.com>
2024-01-12 17:40:15 +0600

    first commit

--- db_module.py
+++ db_module.py
@@ -1,38 +1,22 @@
-# db_module.py
-
 import sqlite3
-
-def create_table():
-    connection = sqlite3.connect('your_database_name.db')
-    cursor = connection.cursor()
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY,
-            username TEXT,
-            first_name TEXT,
-            last_name TEXT
-        )
-    ''')
-
-    connection.commit()
-    connection.close()
 
 def insert_user(user_data):
     connection = sqlite3.connect('your_database_name.db')
     cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO users (user_id, username, first_name, last_name) VALUES (?, ?, ?, ?)', user_data)
+    # Проверяем, существует ли пользователь с таким user_id
+    existing_user = cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_data[0],)).fetchone()
 
-    connection.commit()
-    connection.close()
-
-def record_survey_response(user_id, selected_option):
-    connection = sqlite3.connect('your_database_name.db')
-    cursor = connection.cursor()
-
-    # Здесь реализуйте логику записи ответа на опрос в базу данных
-    # cursor.execute('INSERT INTO survey_responses (user_id, selected_option) VALUES (?, ?)', (user_id, selected_option))
+    if existing_user:
+        # Если пользователь существует, обновляем его данные
+        cursor.execute('''
+            UPDATE users
+            SET username = ?, first_name = ?, last_name = ?
+            WHERE user_id = ?
+        ''', (user_data[1], user_data[2], user_data[3], user_data[0]))
+    else:
+        # Если пользователь не существует, вставляем нового пользователя
+        cursor.execute('INSERT INTO users (user_id, username, first_name, last_name) VALUES (?, ?, ?, ?)', user_data)
 
     connection.commit()
     connection.close()
