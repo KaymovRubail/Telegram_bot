a87b8735a14f9d3b9ce356d2186288b05126bad0
KaymovRubail <ibragimovjoni593@gmail.com>
2024-01-16 02:20:22 +0600

    first commit

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import ParseMode
 from decouple import config
 
-TOKEN = config("TOKEN")
+TOKEN = config("6815811179:AAFKbcyFoNsAyydxU8Pxg5NeB39Gb3x8eOs")
 
 bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(bot)
